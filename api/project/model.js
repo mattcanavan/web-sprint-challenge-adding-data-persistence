f0ac8e9c25fdfe7d95588b4a87bd19d01662ec17
Matt Canavan <mattcanavan@users.noreply.github.com>
2020-12-11 10:34:27 -0800

    Finished project endpoints. Tasks endpoint remaining.

--- api/project/model.js
+++ api/project/model.js
@@ -1,9 +1,21 @@
 // build your `Project` model here
-const db = require('../../data/dbConfig');
+const db = require("../../data/dbConfig");
 
 module.exports = {
-    getAll() {
-
+    getAllProjects() {
+      return db("projects").select("*")
     },
+
+    getProjectById(id) {
+      return db("projects").select("*").where({ id: id })
+    },
+
+    addNewProject(newProjectObj) {
+      let { name, description, completed } = newProjectObj;
+
+      if(!completed) { completed = false} //gotta be a better way
+
+      return db("projects").insert({ name: name, description: description, completed: completed})
+    }
     
   }
--- api/project/router.js
+++ api/project/router.js
@@ -4,5 +4,28 @@ const router = express.Router();
 const HelperFuncs = require('./model');
 
 
+/// ENDPOINTS
+router.get("/", (req,res) => {
+    HelperFuncs.getAllProjects()
+    .then(success => {
+        res.status(200).json(success)
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    })
+})
+
+router.post("/", (req,res) => {
+    //add new project to projects table. (name required, description and completed optional)
+    HelperFuncs.addNewProject(req.body)
+    .then(async success => {
+        //success equals new resource id
+        const newProject = await HelperFuncs.getProjectById(success);
+        res.status(201).json(newProject)
+    })
+    .catch(error => {
+        res.status(500).json({ message: error.message })
+    })
+})
 
 module.exports = router;
